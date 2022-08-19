e37cc93b835b2c86427c96583da1e2802385155f
ysk_univ <ysk.univ.1007@gmail.com>
2022-08-20 07:39:50 +0900

    [lib/mswp/cursor.rb] add

---
+++ lib/mswp/cursor.rb
@@ -0,0 +1,16 @@
+module MSwp
+  class Cursor
+    def initialize(size)
+      @size = size.freeze
+      @pos = Array.new(size.length, 0)
+    end
+
+    def move(dim, delta)
+      return unless (0...@size[dim]) === @pos[dim] + delta
+
+      @pos[dim] += delta
+    end
+
+    attr_reader :pos
+  end
+end
